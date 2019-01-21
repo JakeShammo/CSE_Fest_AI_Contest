c119924cd5f6cd78962a5ea905290198add05fd5
Jacob D'Rozario <iamjacobzz@outlook.com>
2019-01-22 00:15:47 +0600

    Update aicontest_file.py

--- aicontest_file.py
+++ aicontest_file.py
@@ -390,7 +390,7 @@ def display_grid():
             draw_text((-4, 1, 30.0), "Invalid Move by Player" + str(cur_player+1), 64, (120, 120, 220, 255))
             is_over = True
 
-        if check_winner() != -1:
+        if check_winner() == cur_player:
             draw_text((-2.5, 0, 30.0), "Player " + str(check_winner()+1)+" Wins", 64, (120, 120, 220, 255))
             is_over = True
         pygame.display.flip()
